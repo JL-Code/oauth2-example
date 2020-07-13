5253ccd0696de14595bfabfcf8d04fa37bb83301
蒋勇 <jiangy@highzap.com>
2020-07-13 21:16:08 +0800

    add oauth-gateway

---
+++ oauth-gateway/src/main/java/org/example/oauth/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package org.example.oauth.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>创建时间: 2020/7/13 </p>
+ *
+ * @author <a href="mailto:jiangy@highzap.com" rel="nofollow">蒋勇</a>
+ * @version v1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+}
